fd15ff40c0ed007f19df2cc4f11f9bf9a0d90420
Alex Delgado <gadc1996@gmail.com>
2021-02-22 08:16:56 -0700

    Comment create.php

--- create.php
+++ create.php
@@ -1,15 +1,20 @@
 <?php
 session_start();
 include('includes/db-connection.php');
+
+//Values sent from index.php
 $title = $_POST['taskTitle'];
 $description= $_POST['taskDescription'];
 $query = "INSERT INTO tasks(title, description) VALUES('$title', '$description')";
-if($mysqli->query($query) == 1){
+
+//MySQL query
+if($mysqli->query($query) == TRUE ){
 	$_SESSION['message'] = "Query Succesfull";
 }
 else{
 	$_SESSION['message'] = "Query Failed";
 }
+
 //redirect to index 
-header('location: index.php')
+header('location: index.php');
 ?>
